6f8791d83687b4023648ca627ba1ba12e7e55121
Driviz <kumar.driviz@gmail.com>
2020-11-08 02:06:08 +0530

    added index page and controller

---
+++ src/main/java/tech/driviz/recipeproject/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package tech.driviz.recipeproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		return "index";
+	}
+}
