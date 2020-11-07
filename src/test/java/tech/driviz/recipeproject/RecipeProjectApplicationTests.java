4b51689a746c4176ffafd5c59cf4349f1122d64b
Driviz <kumar.driviz@gmail.com>
2020-11-07 23:09:43 +0530

    First Commit

---
+++ src/test/java/tech/driviz/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.driviz.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
