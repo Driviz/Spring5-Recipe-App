85e0fa1796ece93e67842c950490b274ff04bcce
Driviz <kumar.driviz@gmail.com>
2020-11-08 11:53:06 +0530

    added default data using data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (CATEGORY_NAME) VALUES ('American');
+INSERT INTO category (CATEGORY_NAME) VALUES ('Italian');
+INSERT INTO category (CATEGORY_NAME) VALUES ('Mexican');
+INSERT INTO category (CATEGORY_NAME) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (uom) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (uom) VALUES ('Cup');
+INSERT INTO unit_of_measure (uom) VALUES ('Pinch');
+INSERT INTO unit_of_measure (uom) VALUES ('Ounce');
